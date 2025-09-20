a265cfc73f8377449d60f681fe96a30fffb550f5
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-09-20 08:27:29 +0530

    terraform

---
+++ locals/locals.tf
@@ -0,0 +1,6 @@
+locals {
+    domain_name = "dawsconnect.org"
+    zone_id = "Z033712422B61CO3J7OXS"
+    #count.index will not work in locals
+}
+
---
+++ locals/variables.tf
@@ -0,0 +1,12 @@
+variable "instance_names" {
+  type    = list(string)
+  default = ["mysql", "backend", "frontend"]
+}
+
+# variable "zone_id" {
+#   default = "Z033712422B61CO3J7OXS"
+# }
+
+# variable "domain_name" {
+#   default = "dawsconnect.org"
+# }
